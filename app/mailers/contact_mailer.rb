6374ea76b264f1188360e33790a35825f4bfa33e
Robert Phillips <rpdecks@gmail.com>
2025-11-29 21:02:57 -0500

    Update ContactMailer to use verified SendGrid sender email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ApplicationMailer
-  default from: 'noreply@coffeeco.com'
+  default from: ENV['SENDGRID_FROM_EMAIL'] || 'rpdecks@gmail.com'
 
   def contact_form(name:, email:, subject:, message:)
     @name = name
