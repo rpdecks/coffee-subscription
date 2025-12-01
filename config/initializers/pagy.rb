17ade5d64d56171a45b71d3d2d78d32ccf7c1461
Robert Phillips <rpdecks@gmail.com>
2025-11-30 23:12:01 -0500

    Fix Pagy configuration - remove non-existent tailwind extra

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,7 +1,5 @@
 # Pagy Configuration
 require 'pagy/extras/overflow'
-require 'pagy/extras/tailwind'
 
 Pagy::DEFAULT[:items] = 25
 Pagy::DEFAULT[:overflow] = :last_page
-Pagy::DEFAULT[:size] = [1, 2, 2, 1] # Shows: first page, 2 before current, 2 after current, last page
