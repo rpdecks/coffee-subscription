97b9e99aa78cebd7a8999ee683414af9e1bb3b13
Robert Phillips <rpdecks@gmail.com>
2026-02-04 09:29:49 -0500

    Disable SendGrid click tracking in Devise emails

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -8,4 +8,36 @@ class DeviseMailer < Devise::Mailer
   helper :application
 
   default reply_to: ENV.fetch("SUPPORT_EMAIL", "support@acercoffee.com")
+
+  def reset_password_instructions(record, token, opts = {})
+    disable_sendgrid_click_tracking!
+    super
+  end
+
+  def confirmation_instructions(record, token, opts = {})
+    disable_sendgrid_click_tracking!
+    super
+  end
+
+  def unlock_instructions(record, token, opts = {})
+    disable_sendgrid_click_tracking!
+    super
+  end
+
+  private
+
+  def disable_sendgrid_click_tracking!
+    # SendGrid (SMTP) click tracking can rewrite links to a branded tracking domain.
+    # If that domain's TLS is misconfigured, users see a browser cert error.
+    # For authentication flows, keep links direct.
+    headers["X-SMTPAPI"] = {
+      filters: {
+        clicktrack: {
+          settings: {
+            enable: 0
+          }
+        }
+      }
+    }.to_json
+  end
 end
